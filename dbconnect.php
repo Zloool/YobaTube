e26184e0f49a14e00c49f70046214da3d69b05cc
Zloool <zhavoronkov.p@gmail.com>
2015-08-21 14:59:20 +0300

    msqli change

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'settings.php';
 $link = mysqli_connect($mysqlhost, $dblogin, $dbpass,$dbname)
-    or die('Не удалось соединиться: ' . mysql_error());
+    or die('Не удалось соединиться: ' . mysqli_error($link));
 //mysqli_select_db($dbname) or die('Не удалось выбрать базу данных');
 ?>
